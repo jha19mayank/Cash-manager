8da1c4447f8ed85fa2a4ba54e8ad84acf4ddc1e9
jha19mayank <60752983+jha19mayank@users.noreply.github.com>
2021-09-07 10:16:21 +0530

    📝updated code that parses string to number

--- main.js
+++ main.js
@@ -2,17 +2,16 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#amt-check");
 const errMessage = document.querySelector("#error-message");
-const nextButton = document.querySelector("#nxt");
-const cashArea = document.querySelector("cash-area");
-const givingReturn = document.querySelectorAll("return-given");
+const exchangeToReturn = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateAmounts() {
   hideErrorMessage();
   if (billAmount.value > 0) {
-    if (cashGiven.value >= billAmount.value) {
-      const amountToBeReturned = cashGiven.value - billAmount.value;
+    if (Number(cashGiven.value) >= Number(billAmount.value)) {
+      const amountToBeReturned =
+        Number(cashGiven.value) - Number(billAmount.value);
       calculateExhange(amountToBeReturned);
     } else {
       showErrorMessage("Do you wanna wash plates??");
@@ -31,18 +30,11 @@ function showErrorMessage(mail) {
   errMessage.innerText = mail;
 }
 
-nextButton.addEventListener("click", () => {
-  if (billAmount.value) {
-    cashArea.style.display = "block";
-    nextButton.style.display = "none";
-  }
-});
-
 function calculateExhange(amountToBeReturned) {
   for (let i = 0; i < availableNotes.length; i++) {
-    const numberOfNotes = math.trunc(amountToBeReturned / availableNotes[i]);
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
     amountToBeReturned %= availableNotes[i];
-    givingReturn.innerText = numberOfNotes;
+    exchangeToReturn[i].innerText = numberOfNotes;
   }
 }
 
